8f9324f4c867237a437471ffc90d04b523c323ed
shonjs <rush2shon@gmail.com>
2018-02-13 00:23:50 +0530

    CoinFlipper initial commit

--- ENSAccountLookup/index.js
+++ ENSAccountLookup/index.js
@@ -5,11 +5,9 @@ console.log("Current provider", process.env.CONNECTION_MAINNET_HTTP);
 //var provider = new Web3.providers.WebsocketProvider(process.env.CONNECTION_MAINNET_WS);
 var provider = new Web3.providers.HttpProvider(process.env.CONNECTION_MAINNET_HTTP);
 var web3 = new Web3(provider);
-console.log(web3);
-setTimeout(function(){
-	var ens = new ENS(provider);
-	var address = ens.resolver(process.env.SCOTT_ETH).addr()
-					.then(function(addr){
-						console.log(addr);
-					});
-}, 3000);
+var ens = new ENS(provider);
+
+var address = ens.resolver(process.env.SCOTT_ETH).addr()
+				.then(function(addr){
+					console.log(addr);
+				});
